5a8fd999c4d47b474595ec370804d85c638e48b8
AleksanderOrlovIT <alexanderorlovkh@gmail.com>
2023-05-19 16:22:02 +0300

    Added login page, authentication and welcome page.

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,17 +1,37 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    @RequestMapping("/login")
-    public String gotoLoginPage(@RequestParam String name, ModelMap modelMap){
-        System.out.println("Request param is " + name);
-        modelMap.put("name", name);
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private AuthenticationService authenticationService;
+
+
+    public LoginController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @GetMapping("/login")
+        public String gotoLoginPage(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap modelMap){
+        if(authenticationService.authenticate(name, password)) {
+            modelMap.put("name", name);
+            return "welcome";
+        }
+        modelMap.put("errorMessage", "Invalid Credentials! Please try again");
         return "login";
     }
 }
